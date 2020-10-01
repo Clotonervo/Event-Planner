7a2f39d6b5b4769fc9274022671d9a78927ad688
Jacson Tenney <Jacson@Jacsons-iMac.local>
2020-10-01 13:14:19 -0600

    Jacson's Input attempt

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "../components/Layout";
 import Logo from '../components/Common/Logo';
 import { spacing8 } from "../resources/style-constants";
 import styled from "styled-components";
+import JacsonLogin from "../components/Common/Input/jacson"
 
 // Temporary, feel free to delete me
 const LogoWrapper = styled.div`
@@ -14,6 +15,7 @@ const MainPage = () => {
   return (
     <Layout>
       <div>This will be the home page</div>
+      <JacsonLogin>TEST TEST</JacsonLogin> 
       {/* Temporary, feel free to delete me */}
       <LogoWrapper>
         <Logo type={'mark-with-text'}></Logo>
