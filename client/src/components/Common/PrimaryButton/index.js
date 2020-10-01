f8c769f669840160403e355376b39d6fc363b568
Justin Miller <justinmiller@Justins-MacBook-Pro.local>
2020-10-01 13:04:11 -0600

    styled primary button

--- client/src/components/Common/PrimaryButton/index.js
+++ client/src/components/Common/PrimaryButton/index.js
@@ -1,8 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+import {theme1, theme2} from "../../../resources/style-constants"
 
-/** button is colored with the theme color */
-const PrimaryButton = () => {
-  return <div>Button will go here</div>;
+const theme = {
+  teal: {
+    default: theme1,
+    hover: theme2
+  }
 };
 
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  padding: 5px 50px;
+  font-weight: bold;
+  border-radius: 7px;
+  border-width: 0px;
+  width: 30%;
+  outline: 0;
+  cursor: pointer;
+  margin: 10px 0px;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+function clicked() {
+  alert("Proceed to next page");
+}
+
+Button.defaultProps = {
+  theme: "teal"
+};
+
+/** button is colored with the theme color */
+const PrimaryButton = ({title}) => {
+  return <Button onClick = {clicked}>{title}</Button>;
+};
 export default PrimaryButton;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import PrimaryButton from "../components/Common/PrimaryButton"
 
 const MainPage = () => {
   return (
     <Layout>
       <div>This will be the home page</div>
+      <PrimaryButton title = "SIGN IN"></PrimaryButton>
     </Layout>
   );
 };
