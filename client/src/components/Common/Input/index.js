a30f7e9e5082d5ec6d6bd6aa767914ff82626fe4
Morgan Erina Hokanson <Morgan.Hokanson@churchofJesusChrist.org>
2020-10-05 17:50:53 -0600

    Revert "Merge branch 'jacson_branch' into Adding-custom-components-to-login"
    This reverts commit 5d4a2c5f681d4dff4505ad6294ac31a25630716f, reversing
    changes made to fef6fda8bbf789eb511fe02ba00a3226d767efb1.

--- client/src/components/Common/Input/index.js
+++ client/src/components/Common/Input/index.js
@@ -1,31 +1,17 @@
 import React from "react";
 import styled from "styled-components";
-import { spacing16, theme1, grey1, grey2, spacing128, spacing8 } from "../../../resources/style-constants";
+import { spacing8 } from "../../../resources/style-constants";
 
-const StyledInput = styled.input`
-    background-color: white;
-    margin: 0px;
-    padding: ${spacing16};
-<<<<<<< HEAD
-    width: ${ ({fullwidth}) => fullwidth ? "100%" : "40%"}
-=======
-    width: 40%;
->>>>>>> fe51bb251b3fe75b6b66f75fd49089188e665524
-    border-radius: 10px;
-    border-width: 1px;
-    height: 20px;
-    font-size: 20px;
-    
+const Wrapper = styled.div`
+  padding: ${spacing8} 0;
 `;
 
-
-const Input = ({type = "text", placeholder, onChange, fullwidth = false, ...props}) => {
+const Input = () => {
   return (
-    <StyledInput placeholder = {placeholder} type = {type} onChange = {onChange} {...props}/>
-
+    <Wrapper>
+      <input type="text" />
+    </Wrapper>
   );
 };
-
-
 
 export default Input;
