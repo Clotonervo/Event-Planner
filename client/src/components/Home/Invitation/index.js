d50cd3d64cf69bdb832907ad59545d0725e6a923
Josiah <contact@josiahsaunders.com>
2020-10-08 23:26:46 -0600

    invitation flap animation

--- client/src/components/Home/Invitation/index.js
+++ client/src/components/Home/Invitation/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useSpring, animated } from 'react-spring'
 
 /**
  * TODO: Replace this with Justin's card component which
@@ -11,6 +12,7 @@ const TemporaryContainer = styled.div`
   width: 300px;
   border-radius: 8px;
   box-shadow: 0px 10px 8px 0px #888888;
+  cursor: pointer;
 `;
 
 const TriangleFlapContainer = styled.div`
@@ -19,22 +21,49 @@ const TriangleFlapContainer = styled.div`
 
 const TriangleFlap = styled.svg`
   fill: white;
-  -webkit-filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7));
-  filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7));
+  -webkit-filter: drop-shadow( 3px 8px 4px rgba(0, 0, 0, .2));
+  filter: drop-shadow( 3px 8px 4px rgba(0, 0, 0, .2));
 `;
 
 const TriangleFlapPolygon = styled.polygon`
 `;
 
+const getCSSFlapTransformation = (isRotated) => {
+  const rotation = `rotateX(${isRotated ? -40 : 0}deg)`;
+  const trfm = `perspective(400px) ${rotation}`;
+  return trfm;
+}
+
 const Invitation = () => {
+  const [springProps, setSpring] = useSpring(() => ({
+    transform: getCSSFlapTransformation(false),
+    transformOrigin: `50% 0%`,
+    duration: 300,
+    config: {
+      mass: 1,
+      tension: 444,
+      friction: 26,
+    },
+  }))
+
   return (
     <TemporaryContainer
-      onMouseEnter={() => {console.log('entered')}}
-      onMouseLeave={() => {console.log('exited')}}
+      onMouseEnter={() => {
+        console.log(`setting spring ENTER`)
+        setSpring({
+          transform: getCSSFlapTransformation(true),
+        });
+      }}
+      onMouseLeave={() => {
+        console.log(`setting spring EXIT`)
+        setSpring({
+          transform: getCSSFlapTransformation(false),
+        });
+      }}
     >
-      <TriangleFlapContainer>
+      <TriangleFlapContainer as={animated.div} style={springProps}>
         <TriangleFlap width="100%" height="100%" viewBox="0 0 100 100">
-          <TriangleFlapPolygon points="0,0 100,0 50,30" />
+          <TriangleFlapPolygon points="2,2 98,2 50,30" />
         </TriangleFlap>
       </TriangleFlapContainer>
     </TemporaryContainer>
