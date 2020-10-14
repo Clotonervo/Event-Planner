988462db1c390ffca254bcbe17896cd57aa5b609
Josiah <contact@josiahsaunders.com>
2020-10-14 10:25:35 -0600

    functional map

--- client/src/components/Common/Map/index.js
+++ client/src/components/Common/Map/index.js
@@ -3,14 +3,34 @@ import styled from "styled-components";
 import GoogleMapReact from 'google-map-react'
 
 import { borderRadius } from '../../../resources/style-constants'
-import MapPin from '../MapPin';
 
 const Container = styled.div`
+  width: 100%;
+  height: 30vw;
+  border-radius: ${borderRadius};
+  overflow: hidden;
+`;
+
+const MapContainer = styled.div`
   height: 100%;
   width: 100%;
   border-radius: ${borderRadius};
-  background-color: red;
 `;
+
+const InvalidMapContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MapPin = ({ text }) => {
+  return (
+    <div>{text}</div>
+  );
+}
 
 /**
  * @coords 
@@ -21,22 +41,37 @@ const Container = styled.div`
  * }
  */
 const Map = ({ location, zoomLevel }) => {
+  const DEFAULT_LOCATION = { lat: 59.95, lng: 30.33 };
+  const DEFAULT_ZOOM = 11;
+
   console.log(`API: ${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`)
+
+  const map = process.env.REACT_APP_GOOGLE_MAPS_API_KEY !== undefined ?
+    (
+      <MapContainer as={GoogleMapReact}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={location ?? DEFAULT_LOCATION}
+        defaultZoom={zoomLevel ?? DEFAULT_ZOOM}
+      >
+        <MapPin
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </MapContainer>
+    ) :
+    (
+      <InvalidMapContainer>
+        <p>
+          Unable to render map. No API key present.
+        </p >
+      </InvalidMapContainer >
+    );
+
   return (
     <Container>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-      >
-        {/* <MapPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        /> */}
-      </GoogleMapReact>
-      hello
-    </Container >
+      {map}
+    </Container>
   );
 }
 
