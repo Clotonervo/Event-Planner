f792a2898d4e61144a0ce303fd28e768b1db2c8e
Justin Miller <justinamiller1023@gmail.com>
2020-10-15 17:35:18 -0600

    invitee component

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import AppBar from "../Common/AppBar";
 import { eventPink } from "../../resources/style-constants";
+import Invitee from "../../components/Main/Invitee/index.js";
 
 const Main = () => {
   return (
     <div>
       <AppBar color={eventPink} />
       Page coming soon ...
+      <p>
+      </p>
+      <Invitee text = "Barney" rsvpStatus = "declined"></Invitee>
+      <Invitee text = "Ted" rsvpStatus = "accepted"></Invitee>
+      <Invitee text = "Marshall" rsvpStatus = "pending"></Invitee>
     </div>
   );
 };
