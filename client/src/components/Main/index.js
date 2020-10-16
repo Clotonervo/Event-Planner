93c5545ea100804a9eaad31fefbf41a364572d4b
Justin Miller <justinamiller1023@gmail.com>
2020-10-15 18:35:42 -0600

    merged master into this branch

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,20 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-//import AppBar from "../Common/AppBar";
-//import { eventPink } from "../../resources/style-constants";
-=======
 import AppBar from "../Common/AppBar";
 import { eventPink } from "../../resources/style-constants";
->>>>>>> origin/master
 
 const Main = () => {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <AppBar color={eventPink} /> */}
-=======
       <AppBar color={eventPink} />
->>>>>>> origin/master
       Page coming soon ...
     </div>
   );
