53cbc81d28448749ca9a6a669b94dd6e12d49dd5
Morgan Erina Hokanson <morgan.hokanson@gmail.com>
2020-11-09 14:50:01 -0700

    update schemas and database_script

--- server/database_script.js
+++ server/database_script.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const schemas = require('./schemas');
-const fs = require('fs') 
+const mongoose = require("mongoose");
+const schemas = require("./schemas");
+const fs = require("fs");
 
-const User = mongoose.model('User');
-const Event = mongoose.model('Event');
-const Authentication = mongoose.model('Authentication');
+const User = mongoose.model("User");
+const Event = mongoose.model("Event");
+const Authentication = mongoose.model("Authentication");
 
-mongoose.connect('mongodb://localhost:27017/eventplannerDB', {
+mongoose.connect("mongodb://localhost:27017/eventplannerDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -20,203 +20,225 @@ mongoose.connect('mongodb://localhost:27017/eventplannerDB', {
  */
 
 mongoose.connection.on("connected", () => {
-    if(option === "populate"){
-        populateDatabase()
-    }
-    else if (option === "clear"){
-        clearDatabase()
-    }
-    else {
-        console.log("Usage: node test_info.js <option>");
-        console.log("   Options: populate - Populates database with test data");
-        console.log("   Options: clear - Clears the database of all data");
-        process.exit()
-    }
-  });
+  if (option === "populate") {
+    populateDatabase();
+  } else if (option === "clear") {
+    clearDatabase();
+  } else {
+    console.log("Usage: node test_info.js <option>");
+    console.log("   Options: populate - Populates database with test data");
+    console.log("   Options: clear - Clears the database of all data");
+    process.exit();
+  }
+});
 
 var option = process.argv[2];
 
+createTestUser = async function () {
+  const newUser = new User({
+    username: "Test@gmail.com",
+    name: "Test User"
+  });
+  newUser.password = newUser.generateHash("password");
+  const isTestUserThere = await User.findOne({ username: "Test@gmail.com" });
 
-createTestUser = async function() {
-    const newUser = new User({
+  if (isTestUserThere === null) {
+    await newUser.save();
+  }
+};
+
+createTestUserAuthentication = async function () {
+  const testAuth = new Authentication({
+    username: "Test@gmail.com",
+    authToken: "Test",
+    expiration: 0
+  });
+  const userExists = await Authentication.findOne({
+    username: "Test@gmail.com"
+  });
+
+  if (userExists === null) {
+    await testAuth.save();
+  }
+};
+
+createTestUserEvents = async function () {
+  var date = new Date();
+
+  const invitees = [
+    {
+      name: "John Doe",
+      username: "TestUser1",
+      photoUrl: ""
+    },
+    {
+      name: "John Cena",
+      username: "TestUser2",
+      photoUrl: ""
+    },
+    {
+      name: "Joe DiMaggio",
+      username: "TestUser3",
+      photoUrl: ""
+    },
+    {
+      name: "Jerek Deter",
+      username: "TestUser4",
+      photoUrl: ""
+    },
+    {
+      name: "Baseball Guy",
+      username: "TestUser5",
+      photoUrl: ""
+    },
+    {
+      name: "FootBall Fred",
+      username: "TestUser6",
+      photoUrl: ""
+    }
+  ];
+
+  const viewOnlyEvent = new Event({
+    eventID: "12345",
+    title: "View Only Event",
+    viewers: [
+      {
         username: "Test@gmail.com",
-        name:"Test User",
-    })
-    newUser.password = newUser.generateHash("password");
-    const isTestUserThere = await User.findOne({username: "Test@gmail.com"});
-
-    if(isTestUserThere === null){
-        await newUser.save();
+        name: "testName",
+        photoURL: "testurl.com"
+      }
+    ],
+    date: {
+      startDate: date.getDate() + 7,
+      displayDate: "displayDate test1"
+    },
+    invitees: invitees,
+    location: {
+      address: "Provo, UT 84606"
     }
-}
+  });
 
-createTestUserAuthentication = async function() {
-    const testAuth = new Authentication({
+  const Event1 = new Event({
+    eventID: "12346",
+    title: "Wedding",
+    viewers: [
+      {
         username: "Test@gmail.com",
-        authToken: "Test",
-        expiration: 0
-    });
-    const userExists = await Authentication.findOne({username: "Test@gmail.com"});
-
-    if(userExists === null){
-        await testAuth.save();
+        name: "testName",
+        photoURL: "testurl.com"
+      }
+    ],
+    date: {
+      startDate: date.getDate() + 7,
+      displayDate: "displayDate test1"
+    },
+    invitees: invitees,
+    location: {
+      address: "50 N West Temple, Salt Lake City, UT 84150"
     }
-}
+  });
 
-createTestUserEvents = async function() {
-    var date = new Date();
-
-    const invitees =[
-        {
-            name: "John Doe",
-            photoUrl: ""
-        },
-        {
-            name: "John Cena",
-            photoUrl: ""
-        },
-        {
-            name: "Joe DiMaggio",
-            photoUrl: ""
-        },
-        {
-            name: "Jerek Deter",
-            photoUrl: ""
-        },
-        {
-            name: "Baseball Guy",
-            photoUrl: ""
-        },
-        {
-            name: "FootBall Fred",
-            photoUrl: ""
-        },
-    ]
-
-    const viewOnlyEvent = new Event({
-        eventID: "12345",
-        title: "View Only Event",
-        viewers: {
-            username: "Test@gmail.com",
-            name: "testName",
-            photoURL: "testurl.com"
-        },
-        date: {
-            startDate: date.getDate() + 7,
-            displayDate: "displayDate test1"
-        },
-        invitees: invitees
-    });
-    
-    const Event1 = new Event({
-        eventID: "12346",
-        title: "Wedding",
-        viewers: {
-            username: "Test@gmail.com",
-            name: "testName",
-            photoURL: "testurl.com"
-        },
-        date: {
-            startDate: date.getDate() + 7,
-            displayDate: "displayDate test1"
-        },
-        invitees: invitees
-    })
-    
-    const Event2 = new Event({
-        eventID: "12347",
-        title: "Burfday",
-        viewers: {
-            username: "Test@gmail.com",
-            name: "testName",
-            photoURL: "testurl.com"
-        },
-        date: {
-            startDate: date.getDate() + 7,
-            displayDate: "displayDate test1"
-        },
-        invitees: invitees
-    })
-    
-    const Event3 = new Event({
-        eventID: "12348",
-        title: "Funeral",
-        viewers: {
-            username: "Test@gmail.com",
-            name: "testName",
-            photoURL: "testurl.com"
-        },
-        date: {
-            startDate: date.getDate() + 7,
-            displayDate: "displayDate test1"
-        },
-        invitees: invitees
-    })
-
-    var eventExists = await Event.findOne({ eventID: "12345"})
-    // console.log(eventExists);
-    if(eventExists === null){
-        await viewOnlyEvent.save()
+  const Event2 = new Event({
+    eventID: "12347",
+    title: "Burfday",
+    viewers: [
+      {
+        username: "Test@gmail.com",
+        name: "testName",
+        photoURL: "testurl.com"
+      }
+    ],
+    date: {
+      startDate: date.getDate() + 7,
+      displayDate: "displayDate test1"
+    },
+    invitees: invitees,
+    location: {
+      address: "Provo, UT 84606"
     }
+  });
 
-    eventExists = await Event.findOne({ eventID: "12346"})
-    if(eventExists === null){
-        await Event1.save()
+  const Event3 = new Event({
+    eventID: "12348",
+    title: "Funeral",
+    viewers: [
+      {
+        username: "Test@gmail.com",
+        name: "testName",
+        photoURL: "testurl.com"
+      }
+    ],
+    date: {
+      startDate: date.getDate() + 7,
+      displayDate: "displayDate test1"
+    },
+    invitees: invitees,
+    location: {
+      address: "Provo, UT 84606"
     }
+  });
 
-    eventExists = await Event.findOne({ eventID: "12347"})
-    if(eventExists === null){
-        await Event2.save()
-    }
+  var eventExists = await Event.findOne({ eventID: "12345" });
+  // console.log(eventExists);
+  if (eventExists === null) {
+    await viewOnlyEvent.save();
+  }
 
-    eventExists = await Event.findOne({ eventID: "12348"})
-    if(eventExists === null){
-        await Event3.save()
-    }
-    
-}
+  eventExists = await Event.findOne({ eventID: "12346" });
+  if (eventExists === null) {
+    await Event1.save();
+  }
+
+  eventExists = await Event.findOne({ eventID: "12347" });
+  if (eventExists === null) {
+    await Event2.save();
+  }
+
+  eventExists = await Event.findOne({ eventID: "12348" });
+  if (eventExists === null) {
+    await Event3.save();
+  }
+};
 
 async function clearTestUser() {
-    await User.findOneAndDelete({
-        username: "Test@gmail.com"
-    })
+  await User.findOneAndDelete({
+    username: "Test@gmail.com"
+  });
 }
 
-clearTestUserAuthentication = async function() {
-    await Authentication.findOneAndDelete({
-        username: "Test@gmail.com"
-    });
-}
+clearTestUserAuthentication = async function () {
+  await Authentication.findOneAndDelete({
+    username: "Test@gmail.com"
+  });
+};
 
-clearEvents = async function() {
-    await Event.findOneAndDelete({
-        eventID: "12345"
-    })
-    await Event.findOneAndDelete({
-        eventID: "12346"
-    })
-    await Event.findOneAndDelete({
-        eventID: "12347"
-    })
-    await Event.findOneAndDelete({
-        eventID: "12348"
-    })
-}
+clearEvents = async function () {
+  await Event.findOneAndDelete({
+    eventID: "12345"
+  });
+  await Event.findOneAndDelete({
+    eventID: "12346"
+  });
+  await Event.findOneAndDelete({
+    eventID: "12347"
+  });
+  await Event.findOneAndDelete({
+    eventID: "12348"
+  });
+};
 
-populateDatabase = async function() {
-    await createTestUser()
-    await createTestUserAuthentication()
-    await createTestUserEvents()
-    console.log("Database Populated!")
-    process.exit()
-}
+populateDatabase = async function () {
+  await createTestUser();
+  await createTestUserAuthentication();
+  await createTestUserEvents();
+  console.log("Database Populated!");
+  process.exit();
+};
 
-clearDatabase = async function() {
-    await clearTestUser()
-    await clearTestUserAuthentication()
-    await clearEvents()
-    console.log("Database Cleared!");
-    process.exit()
-}
-
-
+clearDatabase = async function () {
+  await clearTestUser();
+  await clearTestUserAuthentication();
+  await clearEvents();
+  console.log("Database Cleared!");
+  process.exit();
+};
--- server/schemas.js
+++ server/schemas.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
 
 /*
 ____________________ Database Schemas _________________________
@@ -10,54 +10,57 @@ ____________________ Database Schemas _________________________
 */
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-  });
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true }
+});
 
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-userSchema.methods.validPassword = function(password) {
-      return bcrypt.compareSync(password, this.password);
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 const authenticationSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true},
-    authToken: { type: String, required: true, unique: true },
-    expiration: { type: Number, required: true }
+  username: { type: String, required: true, unique: true },
+  authToken: { type: String, required: true, unique: true },
+  expiration: { type: Number, required: true }
 });
 
-const Authentication = mongoose.model('Authentication', authenticationSchema);
+const Authentication = mongoose.model("Authentication", authenticationSchema);
 
 const eventSchema = new mongoose.Schema({
-    eventID: { type: String, required: true, unique: true},
-    title: { type: String, required: true},
-    location: [ { address: String } ],
-    collaborators: [{ username: String, name: String, photoURL: String }],
-    viewers: [{ username: String, name: String, photoURL: String }],
-    date: [ { startDate: Date, displayDate: String } ],
-    past: { type: Boolean },
-    description: { type: String },
-    invitees: [{ 
-        name: { type: String },
-        photoUrl: { type: String }
-    }],
-    color: { type: String, default: '#8be4d2' }
+  eventID: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  location: { address: String },
+  collaborators: [{ username: String, name: String, photoURL: String }],
+  viewers: [{ username: String, name: String, photoURL: String }],
+  date: [{ startDate: Date, displayDate: String }],
+  past: { type: Boolean },
+  description: { type: String },
+  invitees: [
+    {
+      username: { type: String },
+      name: { type: String },
+      photoUrl: { type: String }
+    }
+  ],
+  color: { type: String, default: "#8be4d2" }
 });
 
-eventSchema.methods.createEventID = function() {
-      return uuidv4().substring(0, 8)
+eventSchema.methods.createEventID = function () {
+  return uuidv4().substring(0, 8);
 };
 
-const Event = mongoose.model('Event', eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 
 const userToEvents = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    events: [{ type: String }]
+  username: { type: String, required: true, unique: true },
+  events: [{ type: String }]
 });
 
-const UserToEvents = mongoose.model('UserToEvents', userToEvents);
+const UserToEvents = mongoose.model("UserToEvents", userToEvents);
