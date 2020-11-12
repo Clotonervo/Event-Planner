56e4bd266d9c8c521132ae50ccbe54b5add6e035
Morgan Erina Hokanson <morgan.hokanson@gmail.com>
2020-11-11 21:35:21 -0700

    Fix minor issue in layout of home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,11 +3,7 @@ import Layout from "../components/Layout";
 import Main from "../components/Main";
 
 const MainPage = () => {
-  return (
-    <Layout>
-      <Main />
-    </Layout>
-  );
+  return <Main />;
 };
 
 export default MainPage;
