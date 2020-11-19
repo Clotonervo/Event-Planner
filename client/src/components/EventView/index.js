d7229ef797218a82be432ccd5d3de195e29d5710
Josiah <contact@josiahsaunders.com>
2020-11-19 13:07:59 -0700

    event view location component

--- client/src/components/EventView/index.js
+++ client/src/components/EventView/index.js
@@ -4,6 +4,7 @@ import { theme1 } from "../../resources/style-constants";
 import ColorService from "../../services/ColorService";
 import AppBar from "../Common/AppBar";
 import Layout from "../Layout";
+import Location from "./Location";
 
 const EventView = () => {
   const [eventColor, setEventColor] = useState();
@@ -20,8 +21,13 @@ const EventView = () => {
 
   return (
     <div>
-      <AppBar color={eventColor ?? theme1}/>
-      <Layout>Event View</Layout>
+      <AppBar color={eventColor ?? theme1} />
+      <Layout>
+        Event View
+        <Location
+          address="Provo UT" 
+        />
+      </Layout>
     </div>
   );
 };
