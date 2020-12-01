e52d5b3a60e1f7ec3d154e078ea9e05a858cdeea
Justin Miller <justinamiller1023@gmail.com>
2020-12-01 15:20:14 -0700

    created visual aspect of event view page

---
+++ client/src/components/EventView/RSVPCard/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+import Card from "../../Common/Card";
+
+import { borderRadius } from "../../../resources/style-constants";
+
+const CardContainer = styled(Card)`
+  height: 75px;
+  width: 390px;
+  border-radius: ${borderRadius};
+  transform: scale(1.0, 1.0);
+  // TODO: Define shadow constants in style-constants.js and replace this.
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.06), 0 6px 8px 0 rgba(0, 0, 0, 0.06);
+  :active {
+    // TODO: Define shadow constants in style-constants.js and replace this.
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.06), 0 6px 8px 0 rgba(0, 0, 0, 0.06);
+  }
+`;
+
+/**
+ * Reusable card for the home page. Wrap your
+ * component in an <ActionCard> to have consistent
+ * hover, border-radius, size behaviour.
+ */
+const RSVPCard = ({ children, ...props }) => {
+  return (
+    <CardContainer {...{ ...props }}>
+      {children}
+    </CardContainer>
+  );
+};
+
+export default RSVPCard;
--- client/src/components/EventView/index.js
+++ client/src/components/EventView/index.js
@@ -7,6 +7,28 @@ import Layout from "../Layout";
 import YoureInvited from "./YoureInvited";
 import Description from "../../components/EventView/Description";
 import Location from "./Location";
+import RSVPFloating from "../EventView/RSVPFloating/index.js";
+import styled from "styled-components";
+import Invitee from "./InviteesDisplay/Invitee/index.js";
+
+const DisplayStle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 7% 0%;
+`;
+
+const ExtraPadding = styled.div`
+    padding: 4% 0%;
+`;
+
+const InviteeRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  //align-items: center;
+  align-items:flex-start;
+  //justify-content: space-around;
+`;
 
 const EventView = () => {
   const [eventColor, setEventColor] = useState();
@@ -28,11 +50,18 @@ const EventView = () => {
   return (
     <div>
       <AppBar color={eventColor ?? theme1} />
-      <Layout>
-        Event View
-        <Location address="Provo UT" />
+      <Layout><DisplayStle>
+        <RSVPFloating></RSVPFloating></DisplayStle>
         <Description description="The funest party you will ever image. You will have such a blast. Come and have fun in the sun with everyone. Hello keep reading lots of word. Want to make it wrap around to demonstate. Maybe this will do it."></Description>
-      </Layout>
+        <ExtraPadding></ExtraPadding>
+        <Location address="Provo UT" />
+        <ExtraPadding></ExtraPadding>
+        <h1>Who's invited?</h1>
+        <InviteeRow>
+            Invitee Components will go here, but they need a person object or something, so I don't think I can hard code them
+        </InviteeRow>
+       </Layout>
+       <ExtraPadding></ExtraPadding>
     <YoureInvited
       mainText="You're Invited Too!"
       supportingText="Are You Going?"
@@ -40,6 +69,7 @@ const EventView = () => {
       primaryOnClick={invitedButtonClicked}
       secondaryText="No"
       secondaryOnClick={invitedButtonClicked}></YoureInvited>
+      <ExtraPadding></ExtraPadding>
     </div>
   );
 };
